6a66adf4413ce033e468e611aae299758ba41356
Carvo1 <steffanhinkle@gmail.com>
2021-07-11 22:30:53 -0400

    Pushing independant and group files for project

---
+++ module_8/module_10_discussion.py
@@ -0,0 +1,76 @@
+""" 
+    Title: pysports_queries.py
+    Author: Steffan Hinkle
+    Date: 4 July 2021
+    Description: Program queries the teams and players tables of the pysports MySQL database.
+"""
+
+""" import statements """
+import mysql.connector
+from mysql.connector import errorcode
+
+""" database config object """
+config = {
+    "user": "pysports_user",
+    "password": "MySQL8IsGreat!",
+    "host": "127.0.0.1",
+    "database": "pysports",
+    "raise_on_warnings": True
+}
+
+try:
+    """ try/catch block for handling potential MySQL database errors """ 
+    
+    #Connect to the pysports database
+    db = mysql.connector.connect(**config)
+
+    #Creating Cursor object
+    cursor = db.cursor()
+
+    #Selecting fields from team table
+    #cursor.execute("SELECT team_id, team_name, mascot FROM team")
+    cursor.execute("SELECT player_id, first_name, last_name, team_name FROM player p INNER JOIN team t ON t.team_id = p.team_id")
+
+    #Storing results from cursor.execute in variable
+    teamInfo = cursor.fetchall()
+    
+    print("\n -- DISPLAYING TEAM RECORDS --")
+
+    #Iterating through variable storing query results and displaying the output
+    for team in teamInfo:
+        print(" Player_Id: {}\n First Name: {}\n Last Name: {}\n Team Name: {}\n".format(team[0], team[1], team[2], team[3]))
+
+    '''
+    #Selecting fields from player table
+    cursor.execute("SELECT player_id, first_name, last_name, team_id FROM player")
+
+    #Storing results from cursor.execute in variable
+    playerInfo = cursor.fetchall()
+
+    print(playerInfo)
+
+    #print("\n -- DISPLAYING PLAYER RECORDS --")
+
+    #Iterating through variable storing query results and displaying the output
+    #for player in playerInfo:
+    #    print(" Player Id: {}\n First Name: {}\n Last Name {}\n Team Id {}\n".format(player[0], player[1], player[2], player[3]))
+
+    #input("\n\n Press any key to continue...")
+    '''
+
+except mysql.connector.Error as err:
+    """ on error code """
+
+    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+        print("  The supplied username or password are invalid")
+
+    elif err.errno == errorcode.ER_BAD_DB_ERROR:
+        print("  The specified database does not exist")
+
+    else:
+        print(err)
+
+finally:
+    """ close the connection to MySQL """
+
+    db.close()
